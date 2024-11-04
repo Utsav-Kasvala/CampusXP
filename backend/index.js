1b44c9aea302b3246523ad9c49ef88d62edcb996
Isani Alis <isani.alis.mnit@gmail.com>
2024-11-04 16:37:43 +0530

    Completed Assignment feature

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,4 @@
-// //import studentRoutes from "./routes/studentRoutes.js";
-// import express from 'express';
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv'; // Import dotenv
-// import cors from 'cors'; // Import CORS
-// import authRoutes from './routes/Authroutes.js'; // Adjust according to your file structure
-// import classroomRoutes from './routes/ClassroomRoutes.js';
-// import TimeTableRoutes from './routes/TimeTableRoutes.js';
-// import Attendance from './routes/Attendance.js';
-// import assignmentRoute from './routes/assignmentRoute.js'
-// dotenv.config(); // Load environment variables
-
-// const app = express();
-
-// // Middleware
-// app.use(cors()); // Enable CORS
-// app.use(express.json());
-// app.use('/api/v1/auth', authRoutes);
-// app.use('/api/v1/classrooms', classroomRoutes);
-// app.use('/api/v1/timeTable',TimeTableRoutes); 
-// app.use('/api/v1/attendance',Attendance); 
-// app.use('/api/v1/assignments', assignmentRoute);
-// //app.use("/api", studentRoutes);
-// // MongoDB connection
-// mongoose.connect(process.env.MONGO_URL)
-//     .then(() => console.log('MongoDB connected'))
-//     .catch(err => console.error('MongoDB connection error:', err));
-
-// // Start the server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
+import { submitAssignment } from './Controllers/AssignmentController.js';
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'; // Import dotenv
