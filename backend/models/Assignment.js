2bc1c330a2719f06e537e0f3d94a412d118c690c
Isani Alis <isani.alis.mnit@gmail.com>
2024-11-04 16:27:58 +0530

    Completed Assignment feature initial stage

--- backend/models/Assignment.js
+++ backend/models/Assignment.js
@@ -1,23 +1,3 @@
-// import mongoose from 'mongoose';
-
-// const AssignmentSchema = new mongoose.Schema({
-//     title: { type: String, required: true },
-//     description: { type: String },
-//     dueDate: { type: Date, required: true },
-//     totalPoints: { type: Number, default: 100 }, // Total possible points for the assignment
-//     classroom: { type: mongoose.Schema.Types.ObjectId, ref: 'Classroom', required: true }, // Reference to the associated classroom
-//     // professor: { type: mongoose.Schema.Types.ObjectId, ref: 'Professor', required: true }, // Reference to the professor who created the assignment
-//     professor: { type: String, required: true },
-//     submissions: [{
-//         student: { type: mongoose.Schema.Types.ObjectId, ref: 'Student' }, // Reference to the student who submitted
-//         submissionDate: { type: Date },
-//         grade: { type: Number },
-//         feedback: { type: String }
-//     }],
-//     createdAt: { type: Date, default: Date.now }
-// });
-
-// export default mongoose.model('Assignment', AssignmentSchema);
 import mongoose from 'mongoose';
 
 const AssignmentSchema = new mongoose.Schema({
